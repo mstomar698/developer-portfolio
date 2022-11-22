5ac73fe50a07e70438dcc9e77042ba4bc0629ef7
mstomar698 <tomarm698@gmail.com>
2022-11-23 02:31:41 +0530

    adding all other sections

--- javascript/index.js
+++ javascript/index.js
@@ -36,3 +36,102 @@ document.addEventListener('DOMContentLoaded', function () {
       document.querySelector('.messages-section').classList.remove('show');
     });
 });
+
+// Particle JS
+window.onload = function () {
+  Particles.init({
+    selector: '.background',
+    connectParticles: true,
+    color: '#ffffff',
+    sizeVariations: 2,
+    maxParticles: 120,
+    speed: 0.5,
+    minDistance: 100,
+    responsive: [
+      {
+        breakpoint: 768,
+        options: {
+          maxParticles: 100,
+          minDistance: 80,
+          color: '#48F2E3',
+          connectParticles: true,
+        },
+      },
+      {
+        breakpoint: 425,
+        options: {
+          maxParticles: 100,
+          minDistance: 50,
+          connectParticles: true,
+        },
+      },
+      {
+        breakpoint: 320,
+        options: {
+          maxParticles: 0, // disables particles.js
+        },
+      },
+    ],
+  });
+
+  $('.t-input').blur(function () {
+    tmpval = $(this).val();
+    if (tmpval == '') {
+      $(this).addClass('empty');
+      $(this).removeClass('not-empty');
+    } else {
+      $(this).addClass('not-empty');
+      $(this).removeClass('empty');
+    }
+  });
+};
+
+// $(document).ready(function () {
+//   $('.btn-sub').click(function () {
+//     alert('Sorry mate! Form not yet working.');
+//   });
+//   // Smooth Scroll
+//   // Select all links with hashes
+//   $('a[href*="#"]')
+//     // Remove links that don't actually link to anything
+//     .not('[href="#"]')
+//     .not('[href="#0"]')
+//     .click(function (event) {
+//       // On-page links
+//       if (
+//         location.pathname.replace(/^\//, '') ==
+//           this.pathname.replace(/^\//, '') &&
+//         location.hostname == this.hostname
+//       ) {
+//         // Figure out element to scroll to
+//         var target = $(this.hash);
+//         target = target.length
+//           ? target
+//           : $('[name=' + this.hash.slice(1) + ']');
+//         // Does a scroll target exist?
+//         if (target.length) {
+//           // Only prevent default if animation is actually gonna happen
+//           event.preventDefault();
+//           $('html, body').animate(
+//             {
+//               scrollTop: target.offset().top - 90,
+//             },
+//             1000,
+//             function () {
+//               // Callback after animation
+//               // Must change focus!
+//               var $target = $(target);
+//               $target.focus();
+//               if ($target.is(':focus')) {
+//                 // Checking if the target was focused
+//                 return false;
+//               } else {
+//                 $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+//                 $target.focus(); // Set focus again
+//               }
+//             }
+//           );
+//         }
+//       }
+//     });
+// });
