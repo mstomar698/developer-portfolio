bd5353bfa673e69b695485b359981bfa7d57886c
mstomar698 <tomarm698@gmail.com>
2022-11-24 22:23:19 +0530

    Adding all other sections

--- intro.js
+++ intro.js
@@ -1,36 +1,19 @@
-window.onload = function () {
-  Particles.init({
-    selector: '.background',
-    connectParticles: true,
-    color: '#0cf442',
-    sizeVariations: 10,
-    maxParticles: 200,
-    speed: 1,
-    minDistance: 200,
-    responsive: [
-      {
-        breakpoint: 768,
-        options: {
-          maxParticles: 200,
-          minDistance: 80,
-          color: '#48F2E3',
-          connectParticles: true,
-        },
-      },
-      {
-        breakpoint: 425,
-        options: {
-          maxParticles: 100,
-          minDistance: 50,
-          connectParticles: true,
-        },
-      },
-      {
-        breakpoint: 320,
-        options: {
-          maxParticles: 0, 
-        },
-      },
-    ],
-  });
-};
+// skills js
+//  for skill cards
+var listView = document.querySelector('.list-view');
+var gridView = document.querySelector('.grid-view');
+var projectsList = document.querySelector('.project-boxes');
+
+listView.addEventListener('click', function () {
+  gridView.classList.remove('active');
+  listView.classList.add('active');
+  projectsList.classList.remove('jsGridView');
+  projectsList.classList.add('jsListView');
+});
+
+gridView.addEventListener('click', function () {
+  gridView.classList.add('active');
+  listView.classList.remove('active');
+  projectsList.classList.remove('jsListView');
+  projectsList.classList.add('jsGridView');
+});
--- javascript/index.js
+++ javascript/index.js
@@ -43,34 +43,40 @@ document.addEventListener('DOMContentLoaded', function () {
     document.documentElement.classList.toggle('dark');
     modeSwitch.classList.toggle('active');
   });
+  //  for skill cards
+  // var listView = document.querySelector('.list-view');
+  // var gridView = document.querySelector('.grid-view');
+  // var projectsList = document.querySelector('.project-boxes');
 
-  var listView = document.querySelector('.list-view');
-  var gridView = document.querySelector('.grid-view');
+  // listView.addEventListener('click', function () {
+  //   gridView.classList.remove('active');
+  //   listView.classList.add('active');
+  //   projectsList.classList.remove('jsGridView');
+  //   projectsList.classList.add('jsListView');
+  // });
+
+  // gridView.addEventListener('click', function () {
+  //   gridView.classList.add('active');
+  //   listView.classList.remove('active');
+  //   projectsList.classList.remove('jsListView');
+  //   projectsList.classList.add('jsGridView');
+  // });
+  // for project cards
+  var listView2 = document.querySelector('.list-view2');
+  var gridView2 = document.querySelector('.grid-view2');
   var projectsList = document.querySelector('.project-boxes');
 
-  listView.addEventListener('click', function () {
-    gridView.classList.remove('active');
-    listView.classList.add('active');
+  listView2.addEventListener('click', function () {
+    gridView2.classList.remove('active');
+    listView2.classList.add('active');
     projectsList.classList.remove('jsGridView');
     projectsList.classList.add('jsListView');
   });
 
-  gridView.addEventListener('click', function () {
-    gridView.classList.add('active');
-    listView.classList.remove('active');
+  gridView2.addEventListener('click', function () {
+    gridView2.classList.add('active');
+    listView2.classList.remove('active');
     projectsList.classList.remove('jsListView');
     projectsList.classList.add('jsGridView');
   });
-
-  document
-    .querySelector('.messages-btn')
-    .addEventListener('click', function () {
-      document.querySelector('.messages-section').classList.add('show');
-    });
-
-  document
-    .querySelector('.messages-close')
-    .addEventListener('click', function () {
-      document.querySelector('.messages-section').classList.remove('show');
-    });
 });
